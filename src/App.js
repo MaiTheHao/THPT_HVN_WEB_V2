6160b391b71f9750a3e8f3f59aec675b70fb9416
Hào Hay Ke <134072533+MaiTheHao@users.noreply.github.com>
2024-05-05 21:39:09 +0700

    c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, BrowserRouter } from "react-router-dom";
 
 import ICON from "./pic/HVN_banner-trs.jpg";
 import AuthUser from "./components/AuthUser";
@@ -10,24 +10,26 @@ import { routes } from "./Routes";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Navigate to="/home" />}></Route>
-			<Route path="/hvnclient" element={<Navigate to="/home" />}></Route>
-			<Route path="/login/*" element={<Login baseUrl={"/login/"} ICON={ICON} />}></Route>
-			<Route element={<AuthUser />}>
-				{routes.map(({ path, Component, Layout }) => (
-					<Route
-						key={path}
-						path={path}
-						element={
-							<Layout>
-								<Component></Component>
-							</Layout>
-						}
-					></Route>
-				))}
-			</Route>
-		</Routes>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Navigate to="/home" />}></Route>
+				<Route path="/hvnclient" element={<Navigate to="/home" />}></Route>
+				<Route path="/login/*" element={<Login baseUrl={"/login/"} ICON={ICON} />}></Route>
+				<Route element={<AuthUser />}>
+					{routes.map(({ path, Component, Layout }) => (
+						<Route
+							key={path}
+							path={path}
+							element={
+								<Layout>
+									<Component></Component>
+								</Layout>
+							}
+						></Route>
+					))}
+				</Route>
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
 import AppContextProvider from "./Context/AppContextProvider";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <AppContextProvider>
-                <App></App>
-            </AppContextProvider>
-        </BrowserRouter>
-    </React.StrictMode>
+	<React.StrictMode>
+		<AppContextProvider>
+			<App></App>
+		</AppContextProvider>
+	</React.StrictMode>
 );
 
 // Quy ước đặt tên id cho thẻ định danh một component: comp_[name] (name viết hoa chữ cái đầu mỗi từ), VD: comp_Login, comp_ShowSubject.
